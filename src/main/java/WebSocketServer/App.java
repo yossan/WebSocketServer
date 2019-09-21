ac4388702fac370d338f4ae8e53db511f2242566
Kosuke Yoshimoto <kosuke.yoshimot@gmail.com>
2019-09-21 14:45:21 +0900

    🔫 initial commit

---
+++ src/main/java/WebSocketServer/App.java
@@ -0,0 +1,13 @@
+package WebSocketServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
